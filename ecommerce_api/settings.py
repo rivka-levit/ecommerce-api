e0a9dc9cb1d423fc17f31a7002f4d2ba69bee463
rivka-yuliia <juliacreadora@gmail.com>
2023-08-27 18:33:34 +0300

    restframework install

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -29,6 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External Packages
+    'rest_framework',
+    # Internal Apps
 ]
 
 MIDDLEWARE = [
@@ -110,3 +113,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {}
