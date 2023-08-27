7a4c589a9edf3405dbbd9f9e4671e5a0aa4530aa
rivka-yuliia <juliacreadora@gmail.com>
2023-08-27 20:43:14 +0300

    fix templates

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'ecommerce_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
