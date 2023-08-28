e5525e9de315d74c96cd8ddf78b975967f1d0326
rivka-yuliia <juliacreadora@gmail.com>
2023-08-28 17:08:14 +0300

    test for Product model

--- product/tests/test_models.py
+++ product/tests/test_models.py
@@ -16,3 +16,21 @@ class ModelTests(TestCase):
 
         self.assertEqual(brand.name, 'Nike')
         self.assertEqual(str(brand), 'Nike')
+
+    def test_create_product(self):
+        category = models.Category.objects.create(name='Shoes')
+        brand = models.Brand.objects.create(name='Nike')
+        payload = {
+            'name': 'Snickers',
+            'brand': brand,
+            'description': 'Sample snickers description.'
+        }
+        product = models.Product.objects.create(**payload)
+        product.category.add(category)
+
+        for k, v in payload:
+            self.assertEqual(product.getattr(k), v)
+
+        self.assertIn(category, product.category.all())
+        self.assertFalse(product.is_digital)
+        self.assertTrue(product.is_active)
