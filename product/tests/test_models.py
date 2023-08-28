d5ddf5f590e68a78111edc8620d5cb955c89de41
rivka-yuliia <juliacreadora@gmail.com>
2023-08-28 06:13:01 +0300

    brand test, model, admin

--- product/tests/test_models.py
+++ product/tests/test_models.py
@@ -6,13 +6,13 @@ from product import models
 class ModelTests(TestCase):
     """Tests for models."""
     def test_create_category(self):
-        category = models.Category(name='Shoes')
+        category = models.Category.objects.create(name='Shoes')
 
         self.assertEqual(category.name, 'Shoes')
         self.assertEqual(str(category), 'Shoes')
 
     def test_create_brand(self):
-        brand = models.Brand(name='Nike')
+        brand = models.Brand.objects.create(name='Nike')
 
         self.assertEqual(brand.name, 'Nike')
         self.assertEqual(str(brand), 'Nike')
