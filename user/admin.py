50ce7aa557606e5eb6dae7e6c3ab66028d1ac680
rivka-yuliia <juliacreadora@gmail.com>
2023-09-03 08:47:36 +0300

    arrange admin User

--- user/admin.py
+++ user/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'name']
+    fields = ['email', 'name', 'id', 'last_login', 'is_active', 'is_superuser',
+              'is_staff', 'groups', 'user_permissions', 'password']
+    readonly_fields = ['password', 'id', 'last_login']
+    list_filter = ['is_active', 'is_staff']
